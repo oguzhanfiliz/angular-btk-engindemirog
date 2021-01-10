ebc1a596b6916ee3ba5833e60fee51bb94ebf7cb
Oguzhan Filiz <oguzhan.filiz@ogr.dpu.edu.tr>
2021-01-10 19:00:44 +0300

    lesson:15 {{|pipe}}

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,93 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { Product } from './product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() {}
+  title = "Ürün Listesi"
+
+  products: Product[] = [{
+      id: 1,
+      name: "Laptop",
+      price: 15,
+      categoryId: 1,
+      description: "Asus Zenbook",
+      imageUrl: "https://picsum.photos/200/100"
+    }, {
+      id: 2,
+      name: "Mouse",
+      price: 25,
+      categoryId: 2,
+      description: "A4Tech",
+      imageUrl: "https://picsum.photos/200/100"
+    }, {
+      id: 1,
+      name: "Laptop",
+      price: 15,
+      categoryId: 1,
+      description: "Asus Zenbook",
+      imageUrl: "https://picsum.photos/200/100"
+    }, {
+      id: 2,
+      name: "Mouse",
+      price: 25,
+      categoryId: 2,
+      description: "A4Tech",
+      imageUrl: "https://picsum.photos/200/100"
+    }, {
+      id: 1,
+      name: "Laptop",
+      price: 15,
+      categoryId: 1,
+      description: "Asus Zenbook",
+      imageUrl: "https://picsum.photos/200/100"
+    }, {
+      id: 2,
+      name: "Mouse",
+      price: 25,
+      categoryId: 2,
+      description: "A4Tech",
+      imageUrl: "https://picsum.photos/200/100"
+    }, {
+      id: 1,
+      name: "Laptop",
+      price: 15,
+      categoryId: 1,
+      description: "Asus Zenbook",
+      imageUrl: "https://picsum.photos/200/100"
+    }, {
+      id: 2,
+      name: "Mouse",
+      price: 25,
+      categoryId: 2,
+      description: "A4Tech",
+      imageUrl: "https://picsum.photos/200/100"
+    }, {
+      id: 1,
+      name: "Laptop",
+      price: 15,
+      categoryId: 1,
+      description: "Asus Zenbook",
+      imageUrl: "https://picsum.photos/200/100"
+    }, {
+      id: 2,
+      name: "Mouse",
+      price: 25,
+      categoryId: 2,
+      description: "A4Tech",
+      imageUrl: "https://picsum.photos/200/100"
+    },
+
+  ];
+  emptyProducts = "Ürün Listesi boş"
+  ngOnInit(): void {}
+
+}
